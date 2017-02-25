74e53180648ed165cf19b509548c085483388ce3
maiktheknife <maiktheknife@gmail.com>
2017-02-25 16:29:19 +0100

    Init

--- app/src/main/kotlin/net/kivitro/android/preferences/ColorPickerPreference.kt
+++ app/src/main/kotlin/net/kivitro/android/preferences/ColorPickerPreference.kt
@@ -8,85 +8,87 @@ import android.support.v7.preference.PreferenceDialogFragmentCompat
 import android.util.AttributeSet
 import android.view.View
 import com.flask.colorpicker.ColorPickerView
+import preferences.android.kivitro.net.androidpreferences.R
 import timber.log.Timber
 
 /**
  * Created by Max on 29.08.2016.
  */
 class ColorPickerPreference(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : DialogPreference(context, attrs, defStyleAttr, defStyleRes) {
-    var color = 0
-        get
-        set(value) {
-            field = value
-            persistInt(value)
-        }
+	var color = 0
+		get
+		set(value) {
+			field = value
+			persistInt(value)
+		}
 
-    init {
-        isPersistent = true
-        dialogIcon = null
-        dialogLayoutResource = R.layout.pref_colorpicker
+	init {
+		isPersistent = true
+		dialogIcon = null
+		dialogLayoutResource = R.layout.pref_colorpicker
 
-        // layoutResource = R.layout.pref_colorpicker_widget
+		// layoutResource = R.layout.pref_colorpicker_widget
 
-        setPositiveButtonText(android.R.string.ok)
-        setNegativeButtonText(android.R.string.cancel)
-    }
+		setPositiveButtonText(android.R.string.ok)
+		setNegativeButtonText(android.R.string.cancel)
+	}
 
-    constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.attr.preferenceStyle)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, defStyleAttr)
+	constructor(context: Context) : this(context, null)
+	constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.attr.preferenceStyle)
+	constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs, defStyleAttr, defStyleAttr)
 
-    override fun onGetDefaultValue(a: TypedArray, index: Int): Any {
-        Timber.d("onGetDefaultValue")
-        return a.getIndex(index)
-    }
+	override fun onGetDefaultValue(a: TypedArray, index: Int): Any {
+		Timber.d("onGetDefaultValue")
+		return a.getIndex(index)
+	}
 
-    override fun onSetInitialValue(restorePersistedValue: Boolean, defaultValue: Any?) {
-        Timber.d("onSetInitialValue $restorePersistedValue $defaultValue")
-        color = if (restorePersistedValue) getPersistedInt(color) else defaultValue as Int
-    }
+	override fun onSetInitialValue(restorePersistedValue: Boolean, defaultValue: Any?) {
+		Timber.d("onSetInitialValue $restorePersistedValue $defaultValue")
+		color = if (restorePersistedValue) getPersistedInt(color) else defaultValue as Int
+	}
 
-    class ColorPickerPreferenceDialogFragment: PreferenceDialogFragmentCompat(){
-        private lateinit var colorPicker: ColorPickerView
-        private lateinit var colorPreference: ColorPickerPreference
+	class ColorPickerPreferenceDialogFragment : PreferenceDialogFragmentCompat() {
+		private lateinit var colorPicker: ColorPickerView
+		private lateinit var colorPreference: ColorPickerPreference
 
-        override fun onBindDialogView(view: View) {
-            super.onBindDialogView(view)
-            colorPreference = preference as ColorPickerPreference
-            colorPicker = view.findViewById(R.id.color_picker_view) as ColorPickerView
+		override fun onBindDialogView(view: View) {
+			super.onBindDialogView(view)
+			colorPreference = preference as ColorPickerPreference
+			colorPicker = view.findViewById(R.id.color_picker_view) as ColorPickerView
 
-            colorPicker.setColor(colorPreference.color, true)
+			colorPicker.setColor(colorPreference.color, true)
 
-            Timber.d("onBindDialogView ${colorPreference.color}")
-        }
+			Timber.d("onBindDialogView ${colorPreference.color}")
+		}
 
-        override fun onDialogClosed(positiveResult: Boolean) {
-            val selectedValue = colorPicker.selectedColor
-            Timber.d("onDialogClosed $positiveResult $selectedValue")
-            if (positiveResult && colorPreference.callChangeListener(selectedValue)) {
-                colorPreference.color = selectedValue
-            }
-        }
+		override fun onDialogClosed(positiveResult: Boolean) {
+			val selectedValue = colorPicker.selectedColor
+			Timber.d("onDialogClosed $positiveResult $selectedValue")
+			if (positiveResult && colorPreference.callChangeListener(selectedValue)) {
+				colorPreference.color = selectedValue
+			}
+		}
 
-        companion object {
-            fun newInstance(key: String): PreferenceDialogFragmentCompat {
-                Timber.d("newInstance for $key")
-                val f = ColorPickerPreferenceDialogFragment()
-                val b = Bundle(1)
-                b.putString(ARG_KEY, key)
-                f.arguments = b
-                return f
-            }
-        }
+		companion object {
 
-    } // clazz
+			fun newInstance(key: String): PreferenceDialogFragmentCompat {
+				Timber.d("newInstance for $key")
+				val f = ColorPickerPreferenceDialogFragment()
+				val b = Bundle(1)
+				b.putString(ARG_KEY, key)
+				f.arguments = b
+				return f
+			}
+		}
 
-    companion object {
-        const val ns = "http://kivitro.net/android/preferences"
+	} // clazz
 
-        fun newDialogInstance(key: String): PreferenceDialogFragmentCompat {
-            return ColorPickerPreferenceDialogFragment.newInstance(key)
-        }
-    }
+	companion object {
+		const val ns = "http://kivitro.net/android/preferences"
+
+		fun newDialogInstance(key: String): PreferenceDialogFragmentCompat {
+			return ColorPickerPreferenceDialogFragment.newInstance(key)
+		}
+	}
 
 }
